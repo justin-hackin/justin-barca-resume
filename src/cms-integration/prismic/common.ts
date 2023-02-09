8ecceb4c2ed88fb99fed45413f79bd15ca30cc47
Justin Barca <4888331+justin-hackin@users.noreply.github.com>
2023-02-09 16:19:25 -0500

    replace dummy serializer with asHTML

--- src/cms-integration/prismic/common.ts
+++ src/cms-integration/prismic/common.ts
@@ -1,9 +1,10 @@
 import prismic, { getEndpoint } from '@prismicio/client';
+import { asHTML } from '@prismicio/helpers';
 
 const { CMS_REPO_NAME, CMS_ACCESS_TOKEN } = process.env;
 export const cmsClient = prismic.createClient(getEndpoint(CMS_REPO_NAME), {
   accessToken: CMS_ACCESS_TOKEN,
 });
 export const renderToStaticMarkup = function (thing) {
-  return 'prismicContent';
+  return asHTML(thing);
 };
